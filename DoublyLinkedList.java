303dbd680c0633719822918caf7936f9cf5307c3
Saudkhan44 <msaudkhan400@gmail.com>
2025-10-18 03:35:56 -0700

    Adding Doubly and Circular

--- DoublyLinkedList.java
+++ DoublyLinkedList.java
@@ -1,6 +1,7 @@
 package LinkedList;
+import java.util.Scanner;
 
-import java.util.Scanner;// Node for Doubly Linked List
+// Node for Doubly Linked List
 class DNode {
     int data;
     DNode prev, next;
@@ -30,6 +31,28 @@ class DoublyLinkedList {
         newNode.prev = temp;
     }
 
+    void insertAtPosition(int data, int pos) {
+        if (pos <= 1) {
+            insertAtBeginning(data);
+            return;
+        }
+        DNode newNode = new DNode(data);
+        DNode temp = head;
+        int count = 1;
+        while (temp != null && count < pos - 1) {
+            temp = temp.next;
+            count++;
+        }
+        if (temp == null) {
+            insertAtEnd(data);
+        } else {
+            newNode.next = temp.next;
+            if (temp.next != null) temp.next.prev = newNode;
+            temp.next = newNode;
+            newNode.prev = temp;
+        }
+    }
+
     void deleteByValue(int key) {
         if (head == null) return;
         if (head.data == key) {
@@ -47,6 +70,25 @@ class DoublyLinkedList {
         }
     }
 
+    void deleteAtPosition(int pos) {
+        if (head == null || pos < 1) return;
+        if (pos == 1) {
+            head = head.next;
+            if (head != null) head.prev = null;
+            return;
+        }
+        DNode temp = head;
+        int count = 1;
+        while (temp != null && count < pos) {
+            temp = temp.next;
+            count++;
+        }
+        if (temp != null) {
+            if (temp.next != null) temp.next.prev = temp.prev;
+            if (temp.prev != null) temp.prev.next = temp.next;
+        }
+    }
+
     void traverseForward() {
         DNode temp = head;
         while (temp != null) {
@@ -55,42 +97,39 @@ class DoublyLinkedList {
         }
         System.out.println("NULL");
     }
+
+    void traverseBackward() {
+        if (head == null) {
+            System.out.println("NULL");
+            return;
+        }
+        DNode temp = head;
+        while (temp.next != null) temp = temp.next; // Move to tail
+        while (temp != null) {
+            System.out.print(temp.data + " <-> ");
+            temp = temp.prev;
+        }
+        System.out.println("NULL");
+    }
+
+    void search(int key) {
+        if (head == null) {
+            System.out.println("List is empty.");
+            return;
+        }
+        DNode temp = head;
+        int pos = 1;
+        boolean found = false;
+        while (temp != null) {
+            if (temp.data == key) {
+                System.out.println("Value " + key + " found at position " + pos);
+                found = true;
+            }
+            temp = temp.next;
+            pos++;
+        }
+        if (!found) {
+            System.out.println("Value " + key + " not found in the list.");
+        }
+    }
 }
-
-
-
-// Submenu for Doubly Linked List
-//static void doublyMenu(Scanner sc, DoublyLinkedList dll) {
-//    while (true) {
-//        System.out.println("\n--- Doubly Linked List Menu ---");
-//        System.out.println("1. Insert at Beginning");
-//        System.out.println("2. Insert at End");
-//        System.out.println("3. Delete by Value");
-//        System.out.println("4. Traverse Forward");
-//        System.out.println("5. Back to Main Menu");
-//        System.out.print("Enter choice: ");
-//        int ch = sc.nextInt();
-//
-//        switch (ch) {
-//            case 1:
-//                System.out.print("Enter value: ");
-//                dll.insertAtBeginning(sc.nextInt());
-//                break;
-//            case 2:
-//                System.out.print("Enter value: ");
-//                dll.insertAtEnd(sc.nextInt());
-//                break;
-//            case 3:
-//                System.out.print("Enter value to delete: ");
-//                dll.deleteByValue(sc.nextInt());
-//                break;
-//            case 4:
-//                dll.traverseForward();
-//                break;
-//            case 5:
-//                return;
-//            default:
-//                System.out.println("Invalid choice!");
-//        }
-//    }
-//}
